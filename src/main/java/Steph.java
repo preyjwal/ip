a65631b3fead3e05afe1b9bebf3f39f515fa955f
Prakash Prajwal <prajwal020504@gmail.com>
2026-08-19 22:30:26 +0800

    Renamed Chatbot from Duke to Steph

---
+++ src/main/java/Steph.java
@@ -0,0 +1,11 @@
+public class Steph {
+    public static void main(String[] args) {
+        String banner = " ____  _             _     \n"
+                + "/ ___|| |_ ___ _ __ | |__  \n"
+                + "\\___ \\| __/ _ \\ '_ \\| '_ \\ \n"
+                + " ___) | ||  __/ |_) | | | |\n"
+                + "|____/ \\__\\___| .__/|_| |_|\n"
+                + "              |_|\n";
+        System.out.println(banner);
+    }
+}
