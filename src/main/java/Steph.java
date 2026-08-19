991efa2976512e2394dc20bbe5455ddb021b0e4d
Prakash Prajwal <prajwal020504@gmail.com>
2026-08-19 23:11:23 +0800

    Added greeting and exit messages to chatbot

--- src/main/java/Steph.java
+++ src/main/java/Steph.java
@@ -1,11 +1,21 @@
 public class Steph {
+    private static final String NAME = "Steph";
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " ____  _             _     \n"
                 + "/ ___|| |_ ___ _ __ | |__  \n"
                 + "\\___ \\| __/ _ \\ '_ \\| '_ \\ \n"
                 + " ___) | ||  __/ |_) | | | |\n"
                 + "|____/ \\__\\___| .__/|_| |_|\n"
-                + "              |_|\n";
+                + "              |_|";
+
+        System.out.println(LINE);
         System.out.println(banner);
+        System.out.printf("Hello! I'm %s.%nGlad to see you!%nWhat can I help you with?%n", NAME);
+        System.out.println(LINE);
+
+        System.out.println("Goodbye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
